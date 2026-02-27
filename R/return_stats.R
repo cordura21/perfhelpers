52a7c51bf3a981776accd1768a95b9c4f69bc781
cordura21 <cordura21@gmail.com>
2026-02-27 15:09:06 -0300

    renamed main function to return_stats
    Also added Sharpe ratio and Calmar ratio

--- R/return_stats.R
+++ R/return_stats.R
@@ -1,4 +1,4 @@
-#' Annualized Return + Maximum Drawdown Table
+#' Return statistics
 #'
 #' A simple helper that combines `Return.annualized()` and `maxDrawdown()`
 #' from PerformanceAnalytics into one clean table.
@@ -14,21 +14,25 @@
 #' library(PerformanceAnalytics)
 #' data(managers)
 #' R <- managers[, 1:3]
-#' table_return_drawdown(R)
-table_return_drawdown <- function(R, geometric = TRUE, ...) {
+#' return_stats(R)
+return_stats <- function(R, geometric = TRUE, ...) {
   ann_ret <- PerformanceAnalytics::Return.annualized(R, geometric = geometric, ...)
   mdd     <- PerformanceAnalytics::maxDrawdown(R, ...)
   sd <- PerformanceAnalytics::sd.annualized(R,...)
+  sr <- PerformanceAnalytics::SharpeRatio.annualized(R,...)
+  calmar <- PerformanceAnalytics::CalmarRatio(R,)
 
   data.frame(
     Annualized.Return = as.vector(ann_ret),
     Max.Drawdown      = as.vector(mdd),
     SD.Annualized = as.vector(sd),
-    start = xts::first(index(R)),
-    end = xts::last(index(R)),
+    sharpe = as.vector(sr),
+    calmar = as.vector(calmar),
+    start = xts::first(zoo::index(R)),
+    end = xts::last(zoo::index(R)),
     length = nrow(R),
     nas = colSums(is.na(R)),
     periodicity =as.vector( xts::periodicity(R)$scale),
-    row.names         = if (is.null(colnames(R))) "Asset" else colnames(R)
+    row.names = if (is.null(colnames(R))) "Asset" else colnames(R)
   )
 }
--- R/scale_returns.R
+++ R/scale_returns.R
@@ -9,19 +9,14 @@
 #'
 #' @examples
 scale_returns <- function(R, target = NA, ...) {
-  # ... your code ...
+
   curr_vol <- PerformanceAnalytics::sd.annualized(R)
   vol_ratio <- target / as.vector(curr_vol)
+  if(is.na(target)){
+    vol_ratio <- 1
+  }
   R_scaled <- xts::xts(t(t(R) * vol_ratio), zoo::index(R))
   R_scaled
 
 }
 
-#
-# xManagers <- scale_returns(managers,.14)
-# managers / xManagers
-# sd.annualized(managers)
-#
-# xxx <- merge(managers[,9],xManagers[,9])
-# charts.RollingPerformance(xxx,width = 36)
-#
